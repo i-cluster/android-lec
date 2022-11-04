f439cbea04cc834f8e4e30bc1cebdb59e361995b
SSG\212573 <jhlee9091@gmail.com>
2022-11-04 19:01:58 +0900

    221031_Setting
    - Toast
    - Create empty project

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.myButton)
+        val textView = findViewById<TextView>(R.id.myText)
+        var peopleCount = 0
+        button.setOnClickListener {
+            peopleCount += 1
+
+            textView.text = peopleCount.toString()
+            Toast.makeText(this, "It's toast", Toast.LENGTH_LONG).show()
+        }
     }
 }
