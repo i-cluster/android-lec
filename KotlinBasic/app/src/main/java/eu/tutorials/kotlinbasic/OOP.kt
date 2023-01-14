d7401daa9bacefa114613465a40fbd17073e7a4b
i-cluster <jhlee9091@gmail.com>
2023-01-14 22:04:22 +0900

    230114_Object Oriented Programming
    - Data Class
    - Inheritance
    - Interface & Abstract Class
    - Typecasting

--- KotlinBasic/app/src/main/java/eu/tutorials/kotlinbasic/OOP.kt
+++ KotlinBasic/app/src/main/java/eu/tutorials/kotlinbasic/OOP.kt
@@ -1,6 +1,7 @@
 package eu.tutorials.kotlinbasic
 
 import java.lang.IllegalArgumentException
+import kotlin.math.floor
 
 fun main() {
     var holmes = Person("Sherlock", "Holmes", 35)
@@ -15,7 +16,7 @@ fun main() {
     john.hobby = "reading newspaper"
     john.stateHobby()
 
-    var myCar = Car()
+    var myCar = myCar()
     println("Brand is ${myCar.brand}")
     myCar.maxSpeed = 230
     println("Max speed is ${myCar.maxSpeed}")
@@ -45,7 +46,74 @@ fun main() {
     // Destructuring
     val (departedId, departedName) = updatedUesr
     println("id is $departedId and name is $departedName")
+
+    /*
+    * Inheritance
+    */
+    var audiA3 = Car(250.0, "A3", "Audi")
+    var teslaS = ElectricCar(270.0, "S-Model", "Tesla", 85.0)
+
+    teslaS.extendRange(200.0)
+    teslaS.chargetType = "Type2"
+
+    // Polymorphism
+    println(audiA3.drive(200.0))
+    println(teslaS.drive(200.0))
+
+    teslaS.drive()
+
+    audiA3.brake()
+    teslaS.brake()
+
+    teslaS.brake()
+
+    /*
+    * Abstract class
+    */
+    val human = Human("Denis", "England", 70.0, 28.0)
+    val elephant = Elephant("Rosy", "India", 5400.0, 25.0)
+
+    human.run()
+    elephant.run()
+
+    human.breath()
+    elephant.breath()
+
+    /*
+    * TypeCasting
+    */
+    val stringList: List<String> = listOf("Denis", "Frank", "Michael", "Tom")
+    val mixedTypeList: List<Any> = listOf("Denis", 312, 5.0, true)
+
+    // for - if
+    for (value in mixedTypeList) {
+        if (value is Int) {
+            println("Integer : $value")
+        } else if (value is Double) {
+            println("Double: $value with Floor value ${floor(value)}")
+        } else if (value is String) {
+            println("String: $value of length ${value.length}")
+        } else {
+            println("Unknown Type")
+        }
+    }
+
+    // for - when
+    for (value in mixedTypeList) {
+        when (value) {
+            is Int -> println("Integer : $value")
+            is Double -> println("Double: $value with Floor value ${floor(value)}")
+            is String -> println("String: $value of length ${value.length}")
+            else -> println("Unknown Type")
+        }
+    }
+
+    // Typecasting
+    val obj: Any = 1337
+    val str: String? = obj as? String
+    println(str)
 }
+
 
 class Person(firstName: String = "John", lastName: String = "Watson") {
 
@@ -72,7 +140,7 @@ class Person(firstName: String = "John", lastName: String = "Watson") {
     }
 }
 
-class Car() {
+class myCar() {
     lateinit var owner: String
 
     val brand: String = "BMW"
@@ -98,4 +166,90 @@ class Car() {
     }
 }
 
+
 data class User(val id: Long, var name:String)
+
+
+interface Drivable{
+    val maxSpeed: Double
+    fun drive(): String
+    fun brake(){
+        println("The drivable is braking")
+    }
+}
+
+
+// Sub Class, Child Class, Derived Class
+open class Car(override val maxSpeed: Double, val name: String, val brand: String) : Drivable {
+    open var range: Double = 0.0
+
+    fun extendRange(amount: Double) {
+        if(amount > 0)
+            range += amount
+    }
+
+    override fun drive(): String = "driving the interface drive"
+
+    open fun drive(distance: Double) {
+        println("Drive for $distance KM")
+    }
+}
+
+class ElectricCar(override val maxSpeed: Double, name: String, brand: String, batteryLife: Double)
+    : Car(maxSpeed, name, brand) {
+    override var range = batteryLife * 5
+
+    var chargetType = "Type1"
+    override fun drive(distance: Double) {
+        println("Drive for $distance KM on electricity")
+    }
+
+    override fun drive(): String {
+        return "Drive for $range KM on electricity"
+    }
+
+    override fun brake() {
+        // super class
+        super.brake()
+
+        println("brake inside of electric car")
+    }
+}
+
+
+abstract class Mammal(private val name: String, private val origin: String, private val weight: Double) {
+    // Abstract Property (Must be overridden by Subclass)
+    abstract var maxSpeed: Double
+
+    // Abstract Method (Must be implemented by Subclass)
+    abstract fun run()
+    abstract fun breath()
+
+    // Concrete Method
+    fun displayDetails() {
+        println("Name: $name, Origin: $origin, Weight: $weight, Max speed: $maxSpeed")
+
+    }
+}
+
+class Human(name: String, origin: String, weight: Double, override var maxSpeed: Double) :
+    Mammal(name, origin, weight) {
+
+    override fun run() {
+        println("Run on two legs")
+    }
+
+    override fun breath() {
+        println("Breath through mouth or nose")
+    }
+}
+
+class Elephant(name: String, origin: String, weight: Double, override var maxSpeed: Double) : Mammal(name, origin, weight) {
+    override fun run() {
+        println("Run on four legs")
+    }
+
+    override fun breath() {
+        println("Breath through the trunk")
+    }
+}
